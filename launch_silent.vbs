36a518790dfc206c1f0d434a5a41e043a314c24e
Bioinfo User <bioinfo_user@example.com>
2026-03-02 20:24:44 +0800

    Pure Grand Master Release: Cleaned repository of all non-essential and temporary files

--- launch_silent.vbs
+++
@@ -1,2 +0,0 @@
-Set oWS = WScript.CreateObject("WScript.Shell")
-oWS.Run """run_openclaw.bat""", 0, False
--- test_geo.py
+++
@@ -1,9 +0,0 @@
-import GEOparse
-import sys
-
-try:
-    gse = GEOparse.get_GEO(geo="GSE31210", destdir="D:/Bioinfo_Temp_Work", silent=False)
-    print("Download success!", gse.name)
-except Exception as e:
-    import traceback
-    traceback.print_exc()
